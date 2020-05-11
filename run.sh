209ccc445c5e1edf61459993e4f3a635b40e56c3
Matias Käkelä <matias.kakela@relex.fi>
2020-05-11 21:46:17 +0300

    wip

--- run.sh
+++ run.sh
@@ -10,12 +10,14 @@ MQTT_PORT=$(bashio::services mqtt "port")
 MQTT_USER=$(bashio::services mqtt "username")
 MQTT_PASSWORD=$(bashio::services mqtt "password")
 
+HA_DISCOVERY="$(bashio::config 'ha_discovery')"
+HA_DISCOVERY_PREFIX="$(bashio::config 'ha_discovery_prefix')"
+VERBOSE="$(bashio::config 'verbose')"
 
 if [ "$VERBOSE" = "true" ]; then
     set -- "$@" --verbose
 fi
 
-bashio::log.info "Starting $(xcomfortd --version)"
 bashio::log.debug "$(echo $@ | sed s/${MQTT_USER}:${MQTT_PASSWORD}/*****/g)"
 
 exec "$@"
